d7a005551bdd613459f93768054fec10455925cf
Kawika Heftel <kawika@luckykatworld.com>
2020-05-11 19:11:27 -0600

    chore: add building

---
+++ src/TSUtils.ts
@@ -0,0 +1,21 @@
+export default class TSUTils {
+    public static multiAttempt(testFunc:Function, onComplete:Function, numAttempts:number, interval = 50, debug = false) {
+        console.log(`${numAttempts} left`);
+
+        if (numAttempts <= 0) {
+            if (debug) console.log('all attempts failed');
+            return;
+        }
+
+        if (testFunc()) {
+            if (debug) console.log('attempt successful!');
+            onComplete();
+            return;
+        } else {
+            var timeout = setTimeout(function () {
+                if (debug) console.log(`attempting again in ${interval} ms`);
+                TSUTils.multiAttempt(testFunc, onComplete, numAttempts - 1, interval);
+            }, interval);
+        }
+    }
+}
